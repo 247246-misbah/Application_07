4ab47c3f9b07322ef03c6eccb19ae60a165e34b5
247246-misbah <247246@students.au.edu.pk>
2026-05-26 02:26:52 +0500

    Add persistent dark mode toggle layout

--- obj/Debug/net10.0/Application_07.AssemblyInfo.cs
+++ obj/Debug/net10.0/Application_07.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application_07")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df5bec30635e4dd0c2fe6ccf48b6104fb320edd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application_07")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application_07")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
